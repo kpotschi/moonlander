03b349393df37fef5659a2f50cbcee6828e37bf9
Kevin Potschien <k.potschien@sensory-minds.com>
2025-06-23 14:29:47 +0200

    Updates

--- src/app.ts
+++
@@ -1,4 +0,0 @@
-import { phaserConfig } from "./config/phaserConfig";
-import Phaser from "phaser";
-
-const game = new Phaser.Game(phaserConfig);
--- src/systems/debug/debug.d.ts
+++
@@ -1,3 +0,0 @@
-export interface IDebug {
-  debug(gui: GUI): void;
-}
